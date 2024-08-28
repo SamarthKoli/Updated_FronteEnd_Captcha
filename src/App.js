e918af4f9a79466766647dfa8a244c21fbde9408
Priyanka K. Sundalam <ksundalam@gmail.com>
2024-08-29 01:42:21 +0530

    changed some routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Login from './components/Login';
 import Success from './components/Success';
-import Error  from './components/Error';
+import Error from './components/Error';
 
 function App() {
   return (
